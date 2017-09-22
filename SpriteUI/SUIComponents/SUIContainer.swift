d9114ee70569aaffe4e718f64a5e2d8b20deae83
Cacique <phcacique@gmail.com>
2017-09-22 07:01:43 -0300

    First commit on second mac

---
+++ SpriteUI/SUIComponents/SUIContainer.swift
@@ -0,0 +1,17 @@
+//
+//  SUIContainer.swift
+//  SpriteUI
+//
+//  Created by Pedro Cacique on 22/09/17.
+//  Copyright © 2017 Pedro Cacique. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class SUIContainer:SUIComponent{
+    
+    
+    
+    
+}
